2f3baa3803bc2eb85628846cb1e8e42a59380a31
Rhiju Das <rhiju@stanford.edu>
2018-06-03 15:44:57 -0700

    do not fail in edge case of "R" refine in setting anchor lines

--- src/quickpolyxline.m
+++ src/quickpolyxline.m
@@ -3,6 +3,7 @@ function [xsel,ysel,pickedpeak,grayscaleon, maxprof] = quickpolyxline(imx,dontpl
 %   If any number for dontplotthegel is inputed, routine doesn't
 %   replot the gel.
 %    Rhiju Das, August 29 2003.
+% 'R': Refine option from a 2011 function, committed in 2018.
 
 pickedpeak = 0;
 if (nargin<1) hold off;figure(1);image(imx);end;
@@ -20,6 +21,7 @@ count=1;button=0;
 if (nargin>2) xsel(1) = 0; ysel(1) = yselpick; count=2; firstsymbol=plot(1,ysel(1),'ro');end;
 
 finishedline = 0;
+h = {};
 while (finishedline == 0)
     [xsel(count),ysel(count),button]=ginput(1);
     if(button==2) count=count-1; count = max(count,1);
@@ -48,16 +50,20 @@ while (finishedline == 0)
             case {'r','R'}
                 % experimental option, useful for super-strong bands at top
                 % of gel.
-                count = count - 1;
-                set( h{ count } , 'Visible','off');
-                range_min = max(round(ysel(count)) -100,1);
-                range_max = min(round(ysel(count)) +100, size(imx,1));
-                range_bins = [range_min:range_max];
-                [dummy, maxindex ] = max( imx( range_bins, ...
-                    round( xsel(count) ) ) );
-                ysel( count ) = range_bins( maxindex );
-                h{count}= plot([xsel(count-1) xsel(count)],[ysel(count-1) ysel(count)],'r');
-                count = count + 1;
+                if count > 1 & length(h) >= count - 1
+                    count = count - 1;
+                    set( h{ count } , 'Visible','off');
+                    range_min = max(round(ysel(count)) -100,1);
+                    range_max = min(round(ysel(count)) +100, size(imx,1));
+                    range_bins = [range_min:range_max];
+                    xsel
+                    count
+                    [dummy, maxindex ] = max( imx( range_bins, ...
+                        round( xsel(count) ) ) );
+                    ysel( count ) = range_bins( maxindex );
+                    h{count}= plot([xsel(count-1) xsel(count)],[ysel(count-1) ysel(count)],'r');
+                    count = count + 1;
+                end
         end
     end
 end
